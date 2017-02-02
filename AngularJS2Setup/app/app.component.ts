99ee3fec0490cfd31041d0204402a00210b5e4d0
Rashikh <mehtanin.kabir@bd.imshealth.com>
2017-02-02 18:02:11 +0600

    Initial commit.

---
+++ AngularJS2Setup/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template:"<h1> {{welcomeMessage}} </h1>"
+})
+
+export class AppComponent {
+    welcomeMessage: string = "Welcome to my first .NET Angular 2 project!!!!";
+}
